a743c07bd337c5d93d06a9cbec3e21f9ed2a9215
Farrel <farrelathallah06@gmail.com>
2025-06-24 22:13:03 +0800

    initial commit

---
+++ latihan.py
@@ -0,0 +1,34 @@
+print("Welcome")
+
+a = (1, 2, 3)
+b = (4, 5)
+ab = a + b
+print(ab)
+
+c = [10,7,2]
+c.sort()
+print(c)
+
+
+
+dictionary = {   
+    'Nama' : 'HESO',
+    'Umur' : '90'
+}
+
+mencari = str(input(""))
+if mencari in dictionary :
+    print(dictionary.get(mencari))
+else :
+    print(dictionary.get("","data tidak ditemukan"))
+
+
+
+
+
+
+
+
+
+
+
